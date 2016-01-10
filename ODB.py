45f9dc95314c392a7e1b291890a2b27426ccced5
root <wouteroozeleer@gmail.com>
2016-01-10 12:40:46 +0100

    Added a TODO-list and user friendliness

--- ODB.py
+++ ODB.py
@@ -3,7 +3,7 @@
 import urllib2
 import json
 
-def readOpenData():
+def readVilloStations(): # TODO: make user choose between the first three numbers of the data
     url = "https://bruxellesdata.opendatasoft.com/api/records/1.0/search/?dataset=villo-stations-beschikbaarheid-in-real-time&lang=nl&facet=banking&facet=bonus&facet=status&facet=contract_name"
 
     response = urllib2.urlopen(url)
@@ -17,8 +17,23 @@ def readOpenData():
         contract_name = data["records"][x]["fields"]["contract_name"]
         status = data ["records"][x]["fields"]["status"]
 
-        print "%s in %s is %s\n" % (name, contract_name, status)
+        print "\n%s in %s is %s" % (name, contract_name, status)
 	
         x = x + 1
 
-readOpenData()
+def choices(): #TODO: make an array to make this easier
+    print "\n - Villo"
+
+
+print "\n Hi \n Welcome to ODB, the Open Data reader for Brussels.\n For the moment, you can choose between: "
+
+choices()
+
+print "\n Which one do you choose?"
+
+ans = raw_input(">>>   ")
+
+ans = ans.lower()
+
+if ans == "villo":
+    readVilloStations()
