c5fbf561368b19dc540fcebb9112013f69e34e80
root <wouteroozeleer@gmail.com>
2016-01-10 15:15:11 +0100

    You can now choose your station and get more info about it

--- ODB.py
+++ ODB.py
@@ -2,8 +2,10 @@
 
 import urllib2
 import json
+import os
+import sys
 
-def readVilloStations(): # TODO: make user choose between the first three numbers of the data OR use ["number"]
+def readVilloStations(): 
     url = "https://bruxellesdata.opendatasoft.com/api/records/1.0/search/?dataset=villo-stations-beschikbaarheid-in-real-time&lang=nl&facet=banking&facet=bonus&facet=status&facet=contract_name" #Sets url
 
     response = urllib2.urlopen(url) #Reads data from url
@@ -18,28 +20,32 @@ def readVilloStations(): # TODO: make user choose between the first three number
         contract_name = data["records"][x]["fields"]["contract_name"]
         status = data ["records"][x]["fields"]["status"]
 
-        print "\n%s.	%s in %s is %s" % (number, name, contract_name, status)
+        print "\n%s.	%s in %s is %s" % (number, name, contract_name, status) #Display basic info of the stations
 	
-        x = x + 1
+        x = x + 1 #Add to counter called x
 
     print "\n Choose a number to get more info about the station:\n"
-    answer =  raw_input(">>>	")
+    answer =  raw_input(">>>	") #User can now choose a specific station to get more info from it
         
-    counter = 0       
+    counter = 0 #Make second counter so that you can get the chosen number      
  
-    for data["datasetid"] in data["records"]:
+    for data["datasetid"] in data["records"]: #Go through al the data
          
-        number = data["records"][counter]["fields"]["number"]
+        number = data["records"][counter]["fields"]["number"] 
 
-	if float(answer) == float(number):
+	if float(answer) == float(number): #Take the chosen date, given by the answer of the user
             name = data["records"][counter]["fields"]["name"][6:]
             contract_name = data["records"][counter]["fields"]["contract_name"]
             status = data ["records"][counter]["fields"]["status"]
+	    bike_stands = data ["records"][counter]["fields"]["bike_stands"]
+            available_stands = data ["records"][counter]["fields"]["available_bike_stands"]
+	    available_bikes = data ["records"][counter]["fields"]["available_bikes"]
+        
 
-        counter = counter + 1
+	counter = counter + 1
     
 
-    print "\n%s in %s is %s" % (name, contract_name, status)
+    print "\n %s in %s is %s\n\n There are currently %s out of %s bikestands available.\n\n %s bikes are still available\n\n" % (name, contract_name, status, available_stands, bike_stands, available_bikes)
 
 ########END readVilloStations function##########
 
@@ -48,6 +54,10 @@ def choices(): #TODO: make an array to make this easier
     print "\n - Villo"
 
 
+###Here starts the program
+
+
+os.system('clear')
 print "\n Hi \n Welcome to ODB, the Open Data reader for Brussels.\n For the moment, you can choose between: "
 
 choices()
@@ -60,3 +70,6 @@ ans = ans.lower()
 
 if ans == "villo":
     readVilloStations()
+else:
+    print "The number you've chosen isn't in the list"
+    sys.exit()
